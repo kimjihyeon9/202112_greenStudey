06ffe8ce98db467e5750f544f761b67f3a55aa71
kimjihyeon9 <gs9518@naver.com>
2022-01-26 14:49:14 +0900

    Day31_3

--- Project/src/max_colors/MaxColorPanel.java
+++ Project/src/max_colors/MaxColorPanel.java
@@ -1,37 +1,50 @@
 package max_colors;
 
-import java.awt.Graphics;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class MaxColorPanel extends JPanel {
-//	JPanel jPan = new JPanel();
+	private ImageIcon bgImg;
+	private JLabel bgImgPan;
+	
+	private ImageIcon bgSK;
+	private JLabel bgSKPan;
+	
+	private JButton btn1;
+	private JButton btn2;
+	private JButton btn3;
+	
+	private JLabel txt;
 	
 	public MaxColorPanel() {
 		this.setLayout(null);
 		
-		ImageIcon bgSK = new ImageIcon("F:\\JH\\자바_12월\\Project\\src\\select_color_swing\\sk.png");
-		JLabel bgSKPan = new JLabel(bgSK);
+		bgImg = new ImageIcon("F:\\JH\\자바_12월\\Project\\images\\gamebg.png");
+		bgImgPan = new JLabel(bgImg);
+		bgImgPan.setSize(1024, 768);
+
+		bgSK = new ImageIcon("F:\\JH\\자바_12월\\Project\\images\\sk.png");
+		bgSKPan = new JLabel(bgSK);
 		bgSKPan.setBounds(150, 150, 720, 425);
 		
-		JButton btn1 = new JButton("0번째 버튼");
-		JButton btn2 = new JButton("1번째 버튼");
-		JButton btn3 = new JButton("2번째 버튼");
+		btn1 = new JButton("0번째 버튼");
+		btn2 = new JButton("1번째 버튼");
+		btn3 = new JButton("2번째 버튼");
 		btn1.setBounds(500, 100, 100, 50);
 		btn2.setBounds(500, 200, 100, 50);
 		btn3.setBounds(500, 300, 100, 50);
 		
-		JLabel txt = new JLabel("파란색을 눌러주세요!");
+		txt = new JLabel("파란색을 눌러주세요!");
 		txt.setBounds(300, 50, 200, 100);
 		
 		bgSKPan.add(txt);
 		bgSKPan.add(btn1);
 		bgSKPan.add(btn2);
 		bgSKPan.add(btn3);
-		this.add(bgSKPan);
+		bgImgPan.add(bgSKPan);
+		this.add(bgImgPan);
 	}
 
 }
